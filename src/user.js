38f25a4dbcae47270c9b71723e48a662c401abce
Devashish2910 <dgpatel2910@gmail.com>
2018-02-23 02:07:40 -0600

    added: Users collection

---
+++ src/user.js
@@ -0,0 +1,18 @@
+// Create a User Collection
+
+// load mongoose
+const mongoose = require('mongoose');
+// Schema for the collection
+const Schema = mongoose.Schema;
+
+// Schema set up
+const UserSchema = new Schema({
+  //Properties: types of properties
+  name: String
+});
+
+// Create a model
+const Users = mongoose.model('users', UserSchema);
+
+// Export User to use it
+module.exports = Users;
