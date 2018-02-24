e545ab977b3f8d71ceeeacf201d290121d22a1fc
Devashish2910 <dgpatel2910@gmail.com>
2018-02-24 06:18:36 -0600

    test case: add subdoc to existing doc

--- test/postSchema_test.js
+++ test/postSchema_test.js
@@ -1,4 +1,4 @@
-// load assert and Users
+// load assert and Users, Post
 const assert = require('assert');
 const Users = require('./../src/user');
 
@@ -16,6 +16,31 @@ describe('Subdocument Test', () => {
           assert(user.posts[0].title === 'First Post');
           done();
         });
+     });
+  });
+
+  it('Add Post to existing User', done => {
+    const deva = new Users({
+      name: 'Devashish',
+      posts: [{title: 'First'}]
+    });
+
+    deva.save()
+     .then(() => {
+       Users.findById(deva._id)
+        .then(user => {
+          user.posts.push({title: 'New'});
+          user.save()
+           .then(() => {
+             Users.findOne({name: 'Devashish'})
+              .then(updatedUser => {
+                console.log(updatedUser);
+                const topEl = updatedUser.posts.length - 1;
+                assert(updatedUser.posts[topEl].title === 'New')
+                done();
+              })
+           })
+        })
      });
   });
 });
