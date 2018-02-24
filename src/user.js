3c0706b51be4c21e3c5f5cb90df5ecbc866af80e
Devashish2910 <dgpatel2910@gmail.com>
2018-02-24 04:31:53 -0600

    added: test cases for prevention of adding records

--- src/user.js
+++ src/user.js
@@ -10,7 +10,8 @@ const UserSchema = new Schema({
   //Properties: types of properties
   name: {
     type: String,
-    required: [true, 'No Name!!']
+    required: [true, 'No Name!!'],
+    minlength: [3, 'Name must be more than 2 char long']
   },
   postCount: Number
 });
--- test/validation_test.js
+++ test/validation_test.js
@@ -11,5 +11,35 @@ describe('Validate Record', () => {
      const {message} = validation.errors.name;
      assert(message === 'No Name!!');
      done();
-   })
+   });
+
+   it('Name Length Check', done => {
+     const user = new User({name: 'D'});
+     const validation = user.validateSync();
+     const {message} = validation.errors.name;
+     assert(message === 'Name must be more than 2 char long')
+     done();
+   });
+
+   it('Stop adding record due to less length', done => {
+     const user = new User({name: 'D'});
+     user.save()
+      .then()
+      .catch(validation => {
+        const {message} = validation.errors.name;
+        assert(message === 'Name must be more than 2 char long')
+        done();
+      });
+   });
+
+   it('Stop adding record due to no name', done => {
+     const user = new User({name: undefined});
+     user.save()
+      .then()
+      .catch(validation => {
+        const {message} = validation.errors.name;
+        assert(message === 'No Name!!')
+        done();
+      });
+   });
 });
